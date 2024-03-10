b73ec2d9a31c3cf036cecb8bd3764bcb1521446a
Oliver Karstoft <orkarstoft@gmail.com>
2024-03-10 11:27:07 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM golang:1.22.0-alpine AS builder
+
+ARG UPX_VERSION=4.2.1-r0
+
+RUN apk update && apk add --no-cache ca-certificates upx=$UPX_VERSION tzdata && update-ca-certificates
+
+WORKDIR /app
+
+COPY go.mod go.sum /app/
+RUN go mod download
+
+RUN adduser \
+  --disabled-password \
+  --gecos "" \
+  --home "/" \
+  --shell "/sbin/nologin" \
+  --no-create-home \
+  --uid 64000 \
+  dnsupdater
+
+COPY . .
+RUN go build -buildvcs=false -tags netgo -trimpath -tags netgo -ldflags="-w -s" -o ./dnsupdater .
+
+RUN upx --best --lzma dnsupdater
+
+FROM scratch
+
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /etc/group /etc/group
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=builder --chown=dnsupdater:dnsupdater /app/dnsupdater /app/dnsupdater
+
+USER dnsupdater:dnsupdater
+
+ENTRYPOINT ["/app/dnsupdater"]
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/digitalocean/godo"
+)
+
+func main() {
+	doToken := os.Getenv("DO_TOKEN")
+	client := godo.NewFromToken(doToken)
+
+	ctx := context.TODO()
+
+	// Get IP address
+	req, err := http.NewRequest("GET", "https://myip.dk", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "curl/8.4.0")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ip := string(body)
+
+	records, _, err := client.Domains.Records(ctx, "karstoft.pro", &godo.ListOptions{WithProjects: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, record := range records {
+		if record.Name == "ha" {
+			if record.Data == ip {
+				fmt.Println("Record is up to date")
+				break
+			}
+			_, _, err := client.Domains.EditRecord(ctx, "karstoft.pro", record.ID, &godo.DomainRecordEditRequest{
+				Data: ip,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("Record updated")
+			break
+		}
+	}
+}
